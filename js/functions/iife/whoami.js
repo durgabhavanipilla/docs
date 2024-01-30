ce529ca3ae1b48d8fff3423c56d91df0a44b9ad6
durgabhavanipilla <durgabhavani.pilla1@gmail.com>
2024-01-30 23:29:30 +0530

    callbackFunction.html completed
    callbackfunction.html completed

---
+++ js/functions/iife/whoami.js
@@ -0,0 +1,16 @@
+( function() {
+    let name = "1";
+    if (name == 0){
+        name = "Admin";
+    }
+    else if(name = "1"){
+        name = "user";
+    }
+    else{
+        name = "2";
+    }
+    console.log("I am " +name+ ".");
+}
+
+)
+();
